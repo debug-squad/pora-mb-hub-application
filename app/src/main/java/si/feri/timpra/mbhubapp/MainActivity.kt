e4ad0e37b721646e5ea8b0eab32efcbfb7bf88b5
Jan Sernec <jan.sernec@gmail.com>
2023-01-07 00:43:09 +0100

    Added MQTT connector, Added netork connectivity status

--- app/src/main/java/si/feri/timpra/mbhubapp/MainActivity.kt
+++ app/src/main/java/si/feri/timpra/mbhubapp/MainActivity.kt
@@ -1,22 +1,25 @@
 package si.feri.timpra.mbhubapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import si.feri.timpra.mbhubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var app: MyApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        app = application as MyApplication
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
@@ -26,10 +29,33 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_simulate
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_simulate
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val spanOffline = binding.spanOffline
+        app.online.observe(this) {
+            // Update status
+            spanOffline.visibility = if (it) View.GONE else View.VISIBLE
+
+            // Reconnect MQTT
+            if (it) app.connect()
+        }
+
+        val spanMqttOffline = binding.spanMqttOffline
+        app.mqttConnected.observe(this) {
+            // Update status
+            spanMqttOffline.visibility = if (it) View.GONE else View.VISIBLE
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        app.disconnect()
     }
 }
--- app/src/main/java/si/feri/timpra/mbhubapp/MyApplication.kt
+++ app/src/main/java/si/feri/timpra/mbhubapp/MyApplication.kt
@@ -3,17 +3,25 @@ package si.feri.timpra.mbhubapp
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.hivemq.client.mqtt.MqttClient
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
 import java.util.*
 
 const val MY_SP_FILE_NAME = "myshared.data"
 const val PREFERENCES_ID = "ID"
+const val MQTT_BROKER_URL = "125202eb8708473eb5d18de1dacaa45a.s2.eu.hivemq.cloud"
+const val MQTT_BROKER_PORT = 8883
+const val MQTT_USERNAME = "mb-hub-client"
+const val MQTT_PASSWORD = "hhKc28G39bYcec7Q"
 
 class MyApplication : Application() {
     private lateinit var sharedPref: SharedPreferences
@@ -23,13 +31,27 @@ class MyApplication : Application() {
     private lateinit var database: FirebaseDatabase
     private lateinit var eventsRef: DatabaseReference
 
+    private lateinit var mqttClient: Mqtt5Client
+
+    private val _online = MutableLiveData<Boolean>().apply { value = false }
+    val online: LiveData<Boolean> = _online
+
+    private val _mqttConnected = MutableLiveData<Boolean>().apply { value = false }
+    val mqttConnected: LiveData<Boolean> = _mqttConnected
+
     override fun onCreate() {
         super.onCreate()
 
+        //
         // Init shared preferences
+        //
+
         sharedPref = getSharedPreferences(MY_SP_FILE_NAME, Context.MODE_PRIVATE)
 
+        //
         // Set client id
+        //
+
         if (!sharedPref.contains(PREFERENCES_ID)) {
             clientID = UUID.randomUUID()
             with(sharedPref.edit()) {
@@ -40,8 +62,61 @@ class MyApplication : Application() {
             clientID = UUID.fromString(sharedPref.getString(PREFERENCES_ID, null)!!)
         }
 
+        //
         // Connect to db
+        //
+
         database = Firebase.database
         eventsRef = database.getReference("events")
+
+        //
+        // Connect to MQTT
+        //
+
+        mqttClient = MqttClient.builder()
+            .useMqttVersion5()
+            .identifier(clientID.toString())
+            .serverHost(MQTT_BROKER_URL)
+            .serverPort(MQTT_BROKER_PORT)
+            .sslWithDefaultConfig()
+            .simpleAuth()
+            .username(MQTT_USERNAME)
+            .password(MQTT_PASSWORD.toByteArray())
+            .applySimpleAuth()
+            .build()
+
+        //
+        // Connectivity
+        //
+
+        getSystemService(ConnectivityManager::class.java)!!.registerDefaultNetworkCallback(object :
+            NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                _online.postValue(true)
+            }
+
+            override fun onLost(network: Network) {
+                _online.postValue(false)
+            }
+        })
+    }
+
+    fun connect() {
+        if (mqttConnected.value!!) return;
+        mqttClient.toAsync().connect().whenCompleteAsync { _, e ->
+            if (e == null) {
+                _mqttConnected.value = true
+            } else {
+                e.printStackTrace()
+                _mqttConnected.value = false
+            }
+        }
+    }
+
+    fun disconnect() {
+        if (!mqttConnected.value!!) return;
+        mqttClient.toAsync().disconnect().whenCompleteAsync { _, _ ->
+            _mqttConnected.value = false
+        }
     }
 }
