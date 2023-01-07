7ac6a28067c0e443f61c137d35d2cc474f3d04fc
Jan Sernec <jan.sernec@gmail.com>
2023-01-07 01:19:45 +0100

    Reformat

--- app/src/test/java/si/feri/timpra/mbhubapp/ExampleUnitTest.kt
+++ app/src/test/java/si/feri/timpra/mbhubapp/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package si.feri.timpra.mbhubapp
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
